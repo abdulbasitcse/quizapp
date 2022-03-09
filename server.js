c37ed9554b263ec009b31684fed8ac48e8abca40
Ubuntu <ubuntu@ip-172-31-16-128.ec2.internal>
2022-03-09 21:39:45 +0000

    sentry Integration

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://6dfe9e4e20234795a4ebbf3d89faf3a2@o1163440.ingest.sentry.io/6251543",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
+
 require('newrelic');
 const express = require('express')
 const app = express()
