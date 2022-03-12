9c2ed8d32e4e4a1b8eab6bab72238f92451283e9
abdul <abdulmcajh@gmail.com>
2022-03-13 04:59:05 +0530

    Implementation of ioredis in users api

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,19 +2,32 @@ var User = require('../models/user')
 const Question = require('../models/question');
 const { compareSync } = require('bcryptjs');
 const req = require('express/lib/request');
+const axios = require("axios");
+
+const Redis = require("ioredis");
+const redis = new Redis();
 
 exports.getAllusers = (req, res) => {
-    User.find({}, (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ msg: "some error!" });
+    const users = req.params.users;
+    redis.get("searchtext", async(error, result) => {
+        if (error) throw error;
+        if (result !== null) {
+          return res.status(200).send({
+              message: `Data from the cache`,
+              data: JSON.parse(result)
+          })
+        } else {
+            const data = await User.find({});
+            redis.set("searchtext", JSON.stringify(data), "ex", 15);
+            return res.status(200).send({
+                message: `Data from the server`,
+                data: data
+            });
         }
-        else {
-            //res.json({ Listofusers: usr });
-            res.status(200).send(result);
-        }
-    })
+      })
 }
+  
+  
 
 exports.user = (req, res, next) => {
     const email = req.params.email;
@@ -25,7 +38,7 @@ exports.user = (req, res, next) => {
         }
         res.status(200).send(result);
     });
-};
+}
 
 
 
@@ -44,7 +57,7 @@ exports.deleteUser = (req, res, next) => {
 
 exports.updateUser = (req, res) => {
     var email = req.params.email
-    User.updateOne({email: email }, { "$set": req.body }, (err, result) => {
+    User.updateOne({ email: email }, { "$set": req.body }, (err, result) => {
         if (err) {
             return res.status(404).send(err);
         }
@@ -59,41 +72,41 @@ exports.addQuestion = (req, res) => {
     {console.log("TRUE")}
     else {console.log("False")} */
 
-   /*  if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
-        console.log('Object missing');
-      } */
+    /*  if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+         console.log('Object missing');
+       } */
 
-    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        return res.status(400).json({"msg":"body is empty"})
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        return res.status(400).json({ "msg": "body is empty" })
     }
-    else{
-    Question.find({ quizid: req.body.quizid }, (err, q) => {
-        console.log(req.body.quizid)
-        if (err) {
-            console.log(err);
-            res.json({ msg: "some error!" });
-        }
-        else {
-            var question = new Question({
-                quizid: req.body.quizid,
-                questionId: q.length + 1,
-                questionText: req.body.questionText,
-                answer: req.body.answer,
-                options: req.body.options
-            })
-            question.save((err, result) => {
-                if (err) {
-                    console.log(err);
-                    res.status(500).json({ msg: "some error! occured while creating" });
-                }
-                else {
-                    //res.status(200).json({ message: "yes question added!!" })
-                    res.status(201).send(result);
-                }
-            })
-        }
-    })
-}
+    else {
+        Question.find({ quizid: req.body.quizid }, (err, q) => {
+            console.log(req.body.quizid)
+            if (err) {
+                console.log(err);
+                res.json({ msg: "some error!" });
+            }
+            else {
+                var question = new Question({
+                    quizid: req.body.quizid,
+                    questionId: q.length + 1,
+                    questionText: req.body.questionText,
+                    answer: req.body.answer,
+                    options: req.body.options
+                })
+                question.save((err, result) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({ msg: "some error! occured while creating" });
+                    }
+                    else {
+                        //res.status(200).json({ message: "yes question added!!" })
+                        res.status(201).send(result);
+                    }
+                })
+            }
+        })
+    }
 }
 
 exports.getAllQuestion = (req, res) => {
